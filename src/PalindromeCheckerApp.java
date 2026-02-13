359dc85641a127357ab164885f5a8c2049014958
sandhiya-s86 <sandhiya.official86@gmail.com>
2026-02-13 10:53:05 +0530

    UC1: Application Entry & Welcome Message

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,11 @@
+/*
+Application entry point
+ */
+
+public class PalindromeCheckerApp {
+    public static void main (String[] args){
+        System.out.println("Welcome to Palindrome checker Management System");
+        System.out.println("Version : 1.0");
+        System.out.println("System initialized Succesfully");
+    }
+} 
