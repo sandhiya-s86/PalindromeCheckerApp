131a45f00aad15dd85cdba05c328052b061e031d
sandhiya-s86 <sandhiya.official86@gmail.com>
2026-02-27 09:45:32 +0530

    UC7-DequeBased

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,27 +1,22 @@
 import java.util.*;
 public class PalindromeCheckerApp {
     public static void main (String[] args){
-        String input = "civic";
-
-        // Create Queue (FIFO)
-        Queue<Character> queue = new LinkedList<>();
-
-        // Create Stack (LIFO)
-        Stack<Character> stack = new Stack<>();
+        String input = "level";
+        Deque<Character> deque = new LinkedList<>();
 
         for (char c : input.toCharArray()) {
-            queue.add(c);   // enqueue
-            stack.push(c);  // push
+            deque.addLast(c);
         }
 
         boolean isPalindrome = true;
 
-        while (!queue.isEmpty()) {
-            if (!queue.remove().equals(stack.pop())) {
+        while (deque.size() > 1) {
+            if (!deque.removeFirst().equals(deque.removeLast())) {
                 isPalindrome = false;
                 break;
             }
         }
+
         System.out.println("Input : " + input);
         System.out.println("Is Palindrome? : " + isPalindrome);
     }
