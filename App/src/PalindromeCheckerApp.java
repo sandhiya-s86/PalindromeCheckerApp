26d59561edc897920fcba799471f80f308f6d543
sandhiya-s86 <sandhiya.official86@gmail.com>
2026-02-27 08:56:20 +0530

    UC3-PalindromeCheckerApp

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,19 +1,23 @@
-/*
-Application entry point
- */
-
+import java.util.Scanner;
 public class PalindromeCheckerApp {
     public static void main (String[] args){
-        String input = "madam";
-        boolean isPalindrome = true;
-        for (int i = 0; i < input.length() / 2; i++) {
+        Scanner scanner = new Scanner(System.in);
 
-            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
-                isPalindrome = false;
-                break;
-            }
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+
+        String reversed = "";
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversed = reversed + input.charAt(i);
         }
-        System.out.println("Input text: " + input);
-        System.out.println("Is it a Palindrome?: " + isPalindrome);
+
+        if (input.equals(reversed)) {
+            System.out.println("The given string is a Palindrome.");
+        } else {
+            System.out.println("The given string is NOT a Palindrome.");
+        }
+
+        scanner.close();
+
     }
 }
