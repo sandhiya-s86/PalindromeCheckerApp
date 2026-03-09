09f8f06cfaca296673953d0798c2996667d9019f
sandhiya-s86 <sandhiya.official86@gmail.com>
2026-03-09 09:24:54 +0530

    UC13-PerformanceComparison

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,4 +1,5 @@
 import java.util.*;
+
 interface PalindromeStrategy {
     boolean checkPalindrome(String s);
 }
@@ -18,16 +19,12 @@ class StackStrategy implements PalindromeStrategy {
     }
 }
 
-// Deque Strategy
 class DequeStrategy implements PalindromeStrategy {
     public boolean checkPalindrome(String s) {
-
         Deque<Character> deque = new ArrayDeque<>();
-
         for (char c : s.toCharArray()) {
             deque.add(c);
         }
-
         while (deque.size() > 1) {
             if (deque.removeFirst() != deque.removeLast()) {
                 return false;
@@ -40,20 +37,30 @@ class DequeStrategy implements PalindromeStrategy {
 public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
+
         System.out.print("Input : ");
         String s = sc.nextLine();
+
         s = s.replaceAll("\\s+", "").toLowerCase();
-        System.out.print("Choose Strategy (1-Stack, 2-Deque): ");
-        int choice = sc.nextInt();
-        PalindromeStrategy strategy;
-        if (choice == 1) {
-            strategy = new StackStrategy();
-        } else {
-            strategy = new DequeStrategy();
-        }
-        boolean result = strategy.checkPalindrome(s);
-        System.out.println("Is Palindrome? : " + result);
+
+        PalindromeStrategy stackStrategy = new StackStrategy();
+        long start1 = System.nanoTime();
+        boolean result1 = stackStrategy.checkPalindrome(s);
+        long end1 = System.nanoTime();
+
+        PalindromeStrategy dequeStrategy = new DequeStrategy();
+        long start2 = System.nanoTime();
+        boolean result2 = dequeStrategy.checkPalindrome(s);
+        long end2 = System.nanoTime();
+
+        System.out.println("Stack Strategy Result : " + result1);
+        System.out.println("Execution Time : " + (end1 - start1) + " ns");
+
+        System.out.println("Deque Strategy Result : " + result2);
+        System.out.println("Execution Time : " + (end2 - start2) + " ns");
+
         sc.close();
     }
 }
