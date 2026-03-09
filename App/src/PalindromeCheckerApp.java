2e118a77baf49e16af9723d48a310568c793b88c
sandhiya-s86 <sandhiya.official86@gmail.com>
2026-03-09 09:07:13 +0530

    UC11-ObjectOriented

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,22 +1,36 @@
 import java.util.Scanner;
+class PalindromeChecker {
+    public boolean checkPalindrome(String s) {
+        int start = 0;
+        int end = s.length() - 1;
+        while (start < end) {
+            if (s.charAt(start) != s.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+}
 public class PalindromeCheckerApp {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
+
         System.out.print("Input : ");
         String s = sc.nextLine();
-        s = s.replaceAll("\\s+", "").toLowerCase();
-        boolean result = check(s, 0, s.length() - 1);
-        System.out.println("Is Palindrome? : " + result);
-        sc.close();
-    }
-    private static boolean check(String s, int start, int end) {
 
-        if (start >= end) {
-            return true;
-        }
-        if (s.charAt(start) != s.charAt(end)) {
-            return false;
-        }
-        return check(s, start + 1, end - 1);
+        // normalize string (ignore spaces and case)
+        s = s.replaceAll("\\s+", "").toLowerCase();
+
+        // create object of PalindromeChecker
+        PalindromeChecker checker = new PalindromeChecker();
+
+        boolean result = checker.checkPalindrome(s);
+
+        System.out.println("Is Palindrome? : " + result);
+
+        sc.close();
     }
 }
